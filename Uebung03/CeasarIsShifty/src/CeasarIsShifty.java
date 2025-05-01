36cfe0915b3275004f9bc44aa1d40dcc691e38c0
aliceinthyme <alice.neuhaus@protonmail.com>
2025-05-01 19:02:48 +0200

    added comments and assertion(s)

--- Uebung03/CeasarIsShifty/src/CeasarIsShifty.java
+++ Uebung03/CeasarIsShifty/src/CeasarIsShifty.java
@@ -9,38 +9,47 @@ public class CeasarIsShifty {
             //encryption
             InputStream stdin = System.in;
             CeasarEncode a = new CeasarEncode(stdin);
-            //OutputStream file = new FileOutputStream();
             Writer write = new FileWriter(file);
             StringBuilder build = new StringBuilder();
+            //set temp variable
             int b = 0;
-            while((b = a.read()) != 17) {
-                if(b != 20) {
+            //encrypt input
+            while((b = a.read()) != 17) {   //checks for linefeed value + 7 since it will be encoded by read and somehow -1 isnt really playing nice
+                if(b != 20) {   //for windows users (the poor things) since they also have to deal with carriage return (what is the the middle ages?? can I pet the horse at least?)
                     System.out.print((char)b);
-                    build.append((char)b);
+                    build.append((char)b);  //throws the chars into a string
                 }
+                assert b > 0;
             }
-            System.out.print("\n");
-            write.write(build.toString());
+            System.out.print("\n"); //this is just an aesthetics thing for stdout
+            write.write(build.toString());  //writes the string to file
+            //close writers and such
+            a.close();
             stdin.close();
             write.flush();
             write.close();
             //System.out.println("1");
         } catch(IOException e) {
-
+            System.out.println("something went wrong");
         }
         try {
+            //decryption
+            //open reader stuff
             InputStream output = new FileInputStream(file);
             BufferedInputStream reading = new BufferedInputStream(output);
             CeasarDecode dec = new CeasarDecode(reading);
+            //set temp variable
             int c = 0;
+            //decrypt input
             while((c = dec.read()) != -1) {
                 System.out.print((char)c);
             }
+            //close writer stuff
             dec.close();
             reading.close();
             output.close();
         } catch(IOException e) {
-
+            System.out.println("something went wrong");
         }
     }
 }
