8147db3ac72fc20dd2b121eaee960715a99b94e0
aliceinthyme <alice.neuhaus@protonmail.com>
2025-05-02 20:30:53 +0200

    ITS ALIVEEEEEEEE

--- Uebung03/CharCount/src/CharCount.java
+++ Uebung03/CharCount/src/CharCount.java
@@ -6,21 +6,24 @@ public class CharCount {
         File output = new File("output.txt");
         try {
             BufferedReader read = new BufferedReader(new FileReader(file));
+            BufferedReader count = new BufferedReader(new FileReader(file));
             Writer write = new FileWriter(output);
             PrintWriter pwrite = new PrintWriter(write);
-            //set temp variable
-            
+            int max = 0;
+            while(count.read() != -1) {
+                max++;
+            }
+            int[] allChars = new int[max];
+            int b = 0;
+            max--;
+            while((b = read.read()) != -1) {
+                allChars[max] = b;
+                max--;
+            }
+            int temp = 0;
             for(int i = 122; i != 65; i--) {
-                int temp = 0;
-                int b = 0;
-                /*
-                 * TODO:
-                 * figure out how to stuff b into a char array somehow
-                 * second buffered reader
-                 */
-                while((b = read.read()) != -1) {
-                    System.out.println(b+" "+i);
-                    if(b == i) {
+                for(int j = 0; j < allChars.length; j++) {
+                    if(allChars[j] == i) {
                         temp++;
                     }
                 }
@@ -28,22 +31,25 @@ public class CharCount {
                     String a = "The letter "+(char)i+" appears 1 time.";
                     System.out.println(a);
                     pwrite.println(a);
-
+    
                 } else if(temp > 1) {
                     String a = "The letter "+(char)i+" appears "+temp+" times.";
                     System.out.println(a);
                     pwrite.println(a);
                 }
+                temp = 0;
             }
             pwrite.flush();
             write.flush();
             pwrite.close();
             write.close();
             read.close();
+            count.close();
         } catch(IOException e) {
             System.out.println("Something went wrong");
         } catch(ArrayIndexOutOfBoundsException e) {
             System.out.println("Please actually input something!");
+            System.out.println(e.getCause());
         }
     }
 }
